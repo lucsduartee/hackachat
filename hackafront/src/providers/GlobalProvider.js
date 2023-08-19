b90e7adfc29637ba4c7868f6e2efab48170bd524
Lucas Duarte <lucs.rkt@gmail.com>
2023-08-19 17:41:13 -0300

    feat: Improve docker setup

---
+++ hackafront/src/providers/GlobalProvider.js
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react';
+
+export const globalContext = createContext();
+
+export default function GlobalProvider({ children }) {
+  const [courses, setCourses] = useState([]);
+
+  return (
+    <globalContext.Provider value={{ courses, setCourses }}>
+      {children}
+    </globalContext.Provider>
+  );
+};
